027a39ba5bf33a3d9edbb374357d59f6e6e628e9
michaelmg32 <michaelgungun45@gmail.com>
2026-06-06 05:45:09 +0700

    Remove Tailwind CDN to prevent CSS conflict with compiled assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,6 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
